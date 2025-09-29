f484d24abe0cd3655c4724e47f6a5201b196b49a
vOlive2.0 <OLiveA148@lpsk12.org>
2025-09-29 08:46:16 -0600

    A

--- src/MusicRunner.java
+++ src/MusicRunner.java
@@ -1,9 +1,32 @@
-
+import java.io.File;
+import java.util.ArrayList;
+import java.io.IOException;
+import java.util.Scanner;
 public class MusicRunner {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	public static void main(String[] args) throws IOException {
+		File file = new File("playlist.txt");
+		Scanner scan = new Scanner(file);
+		ArrayList<Playlist> playlists = new ArrayList<>();
+		
+		
+		while(scan.hasNextLine()) {
+			String line = scan.nextLine();
+			Scanner lineScanner = new Scanner(line);
+			lineScanner.next();
+			if(line.contains("campsite")) {
+				Campsite c = new Campsite (lineScanner.nextDouble());
+				campObjects.add(c);
+			}
+			else if(line.contains("trail")) {
+				Trail t = new Trail (lineScanner.nextDouble(), lineScanner.nextDouble());
+				campObjects.add(t);
+			}
+		}
+		int area = 0;
+		for(CampObject i : campObjects) {
+			area += i.getArea();
+		}
+		scan.close();
+		System.out.println("Park Area: " + area + ". Thats a Pretty Big Park!");
 	}
-
 }
--- src/Playlist.java
+++ src/Playlist.java
@@ -8,5 +8,9 @@ public class Playlist {
 	public void setName(String n) {name = n;}
 	public ArrayList<Song> getContent() {return content;}
 	public void setContent(ArrayList<Song> c) {content = c;}
+	
+	public ArrayList<Song> remove(int i) {content.remove(i);return content;}
+	public ArrayList<Song> add(Song s) {content.add(s);return content;}
+
 	@Override public String toString() {return name+": \n"+content.toString();}
 }
