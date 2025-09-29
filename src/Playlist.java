4f63924bd14acaa39b67606edb429cfac1d8eb21
vOlive2.0 <OLiveA148@lpsk12.org>
2025-09-29 08:39:21 -0600

    a

--- src/Playlist.java
+++ src/Playlist.java
@@ -1,4 +1,12 @@
+import java.util.ArrayList;
 
 public class Playlist {
-
+	private String name;
+	private ArrayList<Song> content = new ArrayList<>();
+	
+	public String getName() {return name;}
+	public void setName(String n) {name = n;}
+	public ArrayList<Song> getContent() {return content;}
+	public void setContent(ArrayList<Song> c) {content = c;}
+	@Override public String toString() {return name+": \n"+content.toString();}
 }
--- src/Song.java
+++ src/Song.java
@@ -1,4 +1,13 @@
-
 public class Song {
-
+	private String name;
+	private String author;
+	private int year;
+	
+	public String getName() {return name;}
+	public void setName(String n) {name = n;}
+	public String getAuthor() {return author;}
+	public void setAuthor(String a) {author = a;}
+	public int getYear() {return year;}
+	public void setYear(int y) {year = y;}
+	@Override public String toString() {return name+", "+author+", "+year;}
 }
