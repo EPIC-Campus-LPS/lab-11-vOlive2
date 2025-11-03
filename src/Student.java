63389f9b696834f8c82815cae3cdc854bb155d01
vOlive2.0 <OLiveA148@lpsk12.org>
2025-11-03 10:33:26 -0700

    Create Student.java

---
+++ src/Student.java
@@ -0,0 +1,16 @@
+
+public class Student {
+	int Math;
+	int Science;
+	int History;
+	int English;
+	int Computer;
+	public Student(int m, int s, int h, int e, int c) {
+		Math = m;
+		Science = s;
+		History = h;
+		English = e;
+		Computer = c;
+		
+	}
+}
