675fcbdbfff7043f145040b863cbc0773d2526bb
vOlive2.0 <OLiveA148@lpsk12.org>
2025-11-03 10:40:39 -0700

    Update StudentRunner.java

--- src/StudentRunner.java
+++ src/StudentRunner.java
@@ -2,17 +2,26 @@ import java.io.File;
 import java.util.Scanner;
 import java.io.IOException;
 public class StudentRunner {
+	static Student[] kids = new Student[16];
 	public static void main(String[] args) throws IOException {
+		
+	}
+	public static void get(String filename, int i) throws IOException {
 		File f = new File(filename);
-		int c = 0;
 		Scanner scan = new Scanner(f);
-		String line;
+		String line = scan.nextLine();
 		while(scan.hasNextLine()) {
-			System.out.print(c);
-			c++;
-			line = scan.nextLine();
-			String[] splitline = line.split(",");
-			seats[Integer.parseInt(splitline[0])][Integer.parseInt(splitline[1])].buySeat();
+			String splitline[] = scan.nextLine().split(": ");
+			int m = Integer.parseInt(splitline[1]);
+			splitline = scan.nextLine().split(": ");
+			int s = Integer.parseInt(splitline[1]);
+			splitline = scan.nextLine().split(": ");
+			int h = Integer.parseInt(splitline[1]);
+			splitline = scan.nextLine().split(": ");
+			int e = Integer.parseInt(splitline[1]);
+			splitline = scan.nextLine().split(": ");
+			int c = Integer.parseInt(splitline[1]);
+			kids[i] = new Student(m, s, h, e, c);
 		}
 		scan.close();
 	}
